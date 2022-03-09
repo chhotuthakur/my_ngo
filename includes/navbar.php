063072ffa083f85e48bd46bbe995f7428430629c
chhotuthakur <npsdahiya@gmail.com>
2022-03-09 18:27:53 +0530

    Initial commit

---
+++ includes/navbar.php
@@ -0,0 +1,49 @@
+<div class="navbar-main">
+
+    <div class="container">
+
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+
+            </button>
+
+            <a class="navbar-brand" href="index.php"><img src="assets/images/sadaka-logo.png" alt=""></a>
+
+        </div>
+
+        <div id="navbar" class="navbar-collapse collapse pull-right">
+
+            <ul class="nav navbar-nav">
+
+                <li><a class="is-active" href="index.php">HOME</a></li>
+                <li><a href="about.php">ABOUT</a></li>
+                <li class="has-child"><a href="#">CAUSES</a>
+
+                    <ul class="submenu">
+                        <li class="submenu-item"><a href="donate.php">Causes list </a></li>
+                        <li class="submenu-item"><a href="mission.php">Single cause </a></li>
+                        <li class="submenu-item"><a href="mission.php">Single cause </a></li>
+                        <li class="submenu-item"><a href="mission.php">Single cause </a></li>
+                    </ul>
+
+                </li>
+                <li><a href="gallery.php">GALLERY</a></li>
+                <li><a href="contact.php">CONTACT</a></li>
+
+            </ul>
+
+        </div> <!-- /#navbar -->
+
+    </div> <!-- /.container -->
+
+</div> <!-- /.navbar-main -->
+
+
+</nav>
+
+</header> <!-- /. main-header -->
